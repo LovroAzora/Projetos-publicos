378d304f21674aa21652a78e5b1f7d1cb382be97
LovroAzora <168092780+LovroAzora@users.noreply.github.com>
2024-05-09 13:38:26 -0300

    Criando o Xadrez
    Criando a classe posição e implementando o xadrez

---
+++ Xadrez/Xadrez/Program.cs
@@ -0,0 +1,15 @@
+﻿using Xadrez.Tabuleiro;
+
+namespace Xadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            posicao p = new posicao(3,4);
+
+            Console.WriteLine("Posição :" + p) ;
+
+        }
+    }
+}
